643f6b213f99af7c695275547b6f2bc09e9c79a7
j-as-o-n <j.istejada29@gmail.com>
2021-02-05 11:16:22 -0800

    Implemented decompression

--- .src/MyDecompress.cpp
+++ .src/MyDecompress.cpp
@@ -25,8 +25,17 @@ int getCount(char arr[], int n, char c) {
 }
 
 // Decompress the bits, write into destination file
-void decompress(string str) {
-    cout << str;
+void decompress(string str, char c) {
+    int n_compressedBits = stoi(str);
+    char bit[2];
+    
+    if(c == '-')
+        bit[0] = '0';
+    else
+        bit[0] = '1';
+    for(int i = 0; i < n_compressedBits; i++)
+        cout << bit[0];
+    //cout << n_compressedBits << c;
     //Cast string into int, determine whether we will write 1s or 0s, loop until we are done writing.
     return;
 }
@@ -41,7 +50,7 @@ void processString(string bits) {
         }
         else {
             int count = getCount(data, sizeof(data), data[i]); // number of digits between '+' or '-'
-            decompress(string(&bits[i+1], &bits[i+count + 1])); //pass in the string of values between '+' or '-'
+            decompress(string(&bits[i+1], &bits[i+count + 1]), data[i]); //string of values between '+' or '-'
             i += count + 1; // Increment i by how much we skipped
         }
     }
